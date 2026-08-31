47f013bdd5b29886c8534f1ddc4480487ad48fe0
Soumik2001 <soumikmondal61116@gmail.com>
2026-08-31 23:18:56 +0530

    chore: initial project setup — vite, react, tailwind v4, tooling

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='bg-black w-full h-screen'>
+      <h2 className='text-white'>App</h2>
+      </div>
+  )
+}
+
+export default App
